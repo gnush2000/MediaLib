f1d1eec45b1814cbf901eab25cfc375a38933a89
gnush2000 <gnush2000@gmail.com>
2018-09-13 13:52:31 -0500

    Initial commit

---
+++ Song.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class Song here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Song
+{
+    // instance variables - replace the example below with your own
+    private int rating;
+    
+    private String title;
+    
+    /**
+     * Constructor for objects of class Song
+     */
+    public Song()
+    {
+        // initialise instance variables
+        rating = 0;
+    }
+
+    //lkjhlkjhk
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return rating + y;
+    }
+}
