072984834a0daee9ebd39abca315f4895c60df10
Themi <Tcm4760@gmail.com>
2018-11-07 13:10:56 -0600

    put write line in song method

--- LoopingMediaLib.java
+++ LoopingMediaLib.java
@@ -5,7 +5,8 @@ public class LoopingMediaLib
         System.out.println(MediaFile.readString());
         for(int i = MediaLib.numSongs; i > 0; i--)
         {
-            System.out.println(("song" + i).getSong());
+            //System.out.println(("song" + i).getTitle() + "|" + ("song" + i).getRating() + "|" + ("song" + i).getPrice());
+            //MediaFile.writeString(("song" + i).getTitle() + "|" + ("song" + i).getRating() + "|" + ("song" + i).getPrice());
         }
     }
 }
