41c0e42cbcb001e39dec88df7b9b620012e7c461
Themi <Tcm4760@gmail.com>
2018-11-08 13:22:56 -0600

    qwerty

--- LoopingMediaLib.java
+++ LoopingMediaLib.java
@@ -2,11 +2,10 @@ public class LoopingMediaLib
 {
     public static void main()
     {
-        System.out.println(MediaFile.readString());
         for(int i = MediaLib.numSongs; i > 0; i--)
         {
-            //System.out.println(("song" + i).getTitle() + "|" + ("song" + i).getRating() + "|" + ("song" + i).getPrice());
-            //MediaFile.writeString(("song" + i).getTitle() + "|" + ("song" + i).getRating() + "|" + ("song" + i).getPrice());
+            String songInfo = MediaFile.readString();
+            System.out.println(songInfo);
         }
     }
 }
