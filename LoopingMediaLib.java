4445a4b6310474c20f163cb6bf37369a83ae16db
gnush2000 <gnush2000@gmail.com>
2018-11-09 12:21:56 -0600

    b

--- LoopingMediaLib.java
+++ LoopingMediaLib.java
@@ -2,17 +2,10 @@ public class LoopingMediaLib
 {
     public static void main()
     {
-<<<<<<< HEAD
-       String
-        
-        for(int i = MediaLib.numSongs; i > 0; i--)
-        {
-           System.out.println(MediaFile.readString());
-=======
-        for(int i = MediaLib.numSongs; i > 0; i--)
+       for(int i = MediaLib.numSongs; i > 0; i--)
         {
             System.out.println(MediaFile.readString());
->>>>>>> cbc7b121c2b91b0bc268c19ba284cc1c390fa9b5
+
         }
        while (songInfo!= null)
        {
