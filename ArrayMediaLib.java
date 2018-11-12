1156a25b9d94bda3e0c8c4a13f1bac7314e15214
Themi <Tcm4760@gmail.com>
2018-11-12 13:12:17 -0600

    j

--- ArrayMediaLib.java
+++ ArrayMediaLib.java
@@ -8,7 +8,10 @@
 public class ArrayMediaLib
 {
    public static void main() {
-   String [] sharingFriends = {"Anne", "Beth", "Edward", "John", "Sophia", "Smudge"};
-       
-    }
+       String [] sharingFriends = {"Anne", "Beth", "Edward", "John", "Sophia", "Smudge"};
+       for (int i = sharingFriends.length; i > 0; i--)
+       {
+           System.out.println(sharingFriends[i]);
+       }
+   }
 }
