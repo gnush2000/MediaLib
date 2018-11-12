eaf605ea95f41f215f30fcd001dcc2123045074e
gnush2000 <gnush2000@gmail.com>
2018-11-12 13:14:28 -0600

    fixed

--- ArrayMediaLib.java
+++ ArrayMediaLib.java
@@ -8,19 +8,15 @@
 public class ArrayMediaLib
 {
    public static void main() {
-<<<<<<< HEAD
+
    String [] sharingFriends = {"Anne", "Beth", "Edward", "John", "Sophia", "Smudge"};
-       for (int i = 0; i<6; i++)
+       for (int i = sharingFriends.length; i>0; i--)
        {
            System.out.println(sharingFriends[i]);
         }
     }
-=======
-       String [] sharingFriends = {"Anne", "Beth", "Edward", "John", "Sophia", "Smudge"};
-       for (int i = sharingFriends.length; i > 0; i--)
-       {
-           System.out.println(sharingFriends[i]);
-       }
-   }
->>>>>>> 1156a25b9d94bda3e0c8c4a13f1bac7314e15214
+
+       
 }
+
+
