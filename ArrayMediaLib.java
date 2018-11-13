0b932c78ab6876ca43c7de79e378ebea4512dc85
Themi <Tcm4760@gmail.com>
2018-11-13 12:36:55 -0600

    total

--- ArrayMediaLib.java
+++ ArrayMediaLib.java
@@ -7,13 +7,20 @@
  */
 public class ArrayMediaLib
 {
+   private static double total;
    public static void main() {
+   
    String [] sharingFriends = {"Anne", "Beth", "Edward", "John", "Sophia", "Smudge"};
+   int[] daysBtwnPurchase = {2, 5, 1, 2, 4, 2, 1, 3};
    for (int i = 0; i < sharingFriends.length; i++)
    {
        System.out.println(sharingFriends[i]);
    }
+   for (int i = 0; i < daysBtwnPurchase.length; i++)
+   {
+       total = total + daysBtwnPurchase[i];
    }
+   System.out.println(total + " days");
+   System.out.println(total / daysBtwnPurchase.length + " avarage days");
 }
-
-
+}
